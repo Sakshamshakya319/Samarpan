33bfeae54b383f788272e38e1452366a378a28b2
Saksham Shakya <saksham.shakya755@gmail.com>
2025-10-31 02:58:50 +0530

    ver3

--- next.config.mjs
+++ next.config.mjs
@@ -6,10 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Skip static generation for dynamic routes
-  experimental: {
-    isrMemoryCacheSize: 0,
-  },
   onDemandEntries: {
     maxInactiveAge: 15000,
     pagesBufferLength: 2,
